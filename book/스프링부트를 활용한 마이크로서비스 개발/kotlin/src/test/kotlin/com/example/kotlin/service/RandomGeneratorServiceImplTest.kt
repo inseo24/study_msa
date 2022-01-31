82ae90fbaae451fa0959700b07d4ff15a44028bc
seoin <befine324@gmail.com>
2022-01-31 16:18:35 +0900

    add : 구현체 작성 전 test code 추가

---
+++ book/스프링부트를 활용한 마이크로서비스 개발/kotlin/src/test/kotlin/com/example/kotlin/service/RandomGeneratorServiceImplTest.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin.service
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class RandomGeneratorServiceImplTest(
+    val randomGeneratorServiceImpl: RandomGeneratorServiceImpl
+) {
+
+    @Test
+    fun generateRandomFactorIsBetweenExpectedLimits() {
+        // 무작위 숫자 생성
+        val randomFactors: List<Int> = (0 until 1000)
+            .map { randomGeneratorServiceImpl.generateRandomFactor() }
+            .toList()
+
+        // 적당히 어려운 계산을 만들기 위해
+        // 생성한 인수가 11~99 범위에 있는지 확인
+        val range : List<Int> = (11 until 100).toList()
+        Assertions.assertThat(randomFactors).containsOnlyOnceElementsOf(range)
+    }
+
+}
