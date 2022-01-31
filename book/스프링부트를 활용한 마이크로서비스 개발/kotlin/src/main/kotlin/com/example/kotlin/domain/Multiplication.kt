a39179bebe0d97414a400a3e1bfe5f4603c10acb
seoin <befine324@gmail.com>
2022-01-31 15:53:54 +0900

    add : 요구사항 테스트 작성

--- book/스프링부트를 활용한 마이크로서비스 개발/kotlin/src/main/kotlin/com/example/kotlin/domain/Multiplication.kt
+++ book/스프링부트를 활용한 마이크로서비스 개발/kotlin/src/main/kotlin/com/example/kotlin/domain/Multiplication.kt
@@ -4,9 +4,9 @@ package com.example.kotlin.domain
 * 애플리케이션에 곱셈을 나타내는 클래스
 * */
 class Multiplication(
-    private var factorA: Int,
-    private var factorB: Int,
-    private var result: Int = factorA * factorB
+    var factorA: Int,
+    var factorB: Int,
+    var result: Int = factorA * factorB
 ) {
     override fun toString(): String {
         return "Multiplication(factorA=$factorA, factorB=$factorB, result(A*B)=$result)"
--- book/스프링부트를 활용한 마이크로서비스 개발/kotlin/src/test/kotlin/com/example/kotlin/service/MultiplicationServiceTest.kt
+++ book/스프링부트를 활용한 마이크로서비스 개발/kotlin/src/test/kotlin/com/example/kotlin/service/MultiplicationServiceTest.kt
@@ -1,4 +1,35 @@
 package com.example.kotlin.service
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+
+@SpringBootTest
 class MultiplicationServiceTest {
+
+    @MockBean
+    lateinit var randomGeneratorService: RandomGeneratorService
+
+    @Autowired
+    lateinit var multiplicationService: MultiplicationService
+
+    @Test
+    fun createRandomMultiplicationTest() {
+        // given (randomGeneratorService 가 처음에는 50, 나중에는 30을 반환하게 설정)
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30)
+
+        // when
+        val multiplication = multiplicationService.createRandomMultiplication()
+
+        // assert
+        assertThat(multiplication.factorA).isEqualTo(50)
+        assertThat(multiplication.factorB).isEqualTo(30)
+        assertThat(multiplication.result).isEqualTo(1500)
+    }
+
 }
+
+
