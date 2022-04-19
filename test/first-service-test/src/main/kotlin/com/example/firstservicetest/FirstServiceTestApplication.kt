268adcee652ff8cd35f31b26447633bd13d1cfc1
choi seoin <inseo@uupfi.com>
2022-04-19 15:08:14 +0900

    config client 에서 application name 을 맞춰주는 걸로 결론 탕탕

--- test/first-service-test/src/main/kotlin/com/example/firstservicetest/FirstServiceTestApplication.kt
+++ test/first-service-test/src/main/kotlin/com/example/firstservicetest/FirstServiceTestApplication.kt
@@ -1,5 +1,6 @@
 package com.example.firstservicetest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
